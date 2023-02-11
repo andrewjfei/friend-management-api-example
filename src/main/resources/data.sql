aa14409b5cc81ef3350ae2682b03a1e8759da3f1
Andrew J Fei <andrewjfei@gmail.com>
2023-02-12 12:42:49 +1300

    NONE - updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,9 @@
 INSERT INTO "user"(id, username, first_name, last_name, password, created) VALUES
 ('11afa5b4-c622-4320-a4e9-7c374172b63d', 'joesmith', 'Joe', 'Smith', 'password', CURRENT_TIMESTAMP),
 ('26770bad-887c-4ef7-a77c-f582d50e201c', 'caseywang', 'Casey', 'Wang', 'password', CURRENT_TIMESTAMP),
-('ee934861-9c00-4795-b78d-0bb760517dd4', 'jaxjones', 'Jax', 'Jones', 'password', CURRENT_TIMESTAMP);
+('ee934861-9c00-4795-b78d-0bb760517dd4', 'jaxjones', 'Jax', 'Jones', 'password', CURRENT_TIMESTAMP),
+('80a71220-e1bb-4b6c-a1b8-6331b7d42983', 'bobgreen', 'Bob', 'Green', 'password', CURRENT_TIMESTAMP),
+('0d7bae2b-0909-427c-ba62-4c74872c710c', 'alexchen', 'Alex', 'Chen', 'password', CURRENT_TIMESTAMP);
 
 INSERT INTO "friendship"(requester_id, receiver_id, is_accepted, created) VALUES
 ('11afa5b4-c622-4320-a4e9-7c374172b63d', '26770bad-887c-4ef7-a77c-f582d50e201c', true, CURRENT_TIMESTAMP),
